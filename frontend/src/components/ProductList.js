d3d1d3ed223ed421e224a65eb1c3d74a33832b17
Abhijindal905 <abhijindal905@gmail.com>
2025-04-18 12:24:22 +0530

    added product list

---
+++ frontend/src/components/ProductList.js
@@ -0,0 +1,25 @@
+import {useEffect, useState} from 'react';
+import { fetchProducts } from '../api';
+
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+
+    useEffect(() => {
+        fetchProducts().then(setProducts)
+    }, []);
+
+    return (
+        <div className="grid">
+          {products.map((product) => (
+            <div key={product.id}>
+              <h3>{product.name}</h3>
+              <p>₹{product.price}</p>
+              <img src={`http://localhost:8000${product.image}`} alt={product.name} width="100" />
+            </div>
+          ))}
+        </div>
+      );
+};
+
+export default ProductList;
