7609ba777063541dfb8e306e339c2ce3199e4065
Abhijindal905 <abhijindal905@gmail.com>
2025-04-19 10:14:17 +0530

    added axios for fetching the data from backend

--- frontend/src/components/screens/HomeScreen.js
+++ frontend/src/components/screens/HomeScreen.js
@@ -1,8 +1,35 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import axios from 'axios'
 function HomeScreen() {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    async function fetchproducts(){
+      const response = await axios.get('http://127.0.0.1:8000/api/products/')
+      const data = response.data
+      console.log(data)
+      setProducts(data)
+    }
+    fetchproducts()
+  }, [])
+
+
   return (
-    <div>HomeScreen</div>
+    <Container>
+      <br/>
+      <h1>The Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <h3>{product.product_name}</h3>
+            <h6>{product.price}</h6>
+            <p>{product.category}</p>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   )
 }
 
