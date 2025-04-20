791f0c763d2e3c774eb0aedfd93d1370d0e8f3df
Abhijindal905 <abhijindal905@gmail.com>
2025-04-21 01:33:57 +0530

    Token added for user authentification

--- backend/ecommerce/ecommapp/urls.py
+++ backend/ecommerce/ecommapp/urls.py
@@ -1,10 +1,14 @@
 from .views import *
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 
 urlpatterns = [
     path('', getRoutes, name= "getRoutes"),
     path('products/', getProducts, name= "getProducts"),
-    path('product/<str:id>', getProduct, name="getProduct")
+    path('product/<str:id>', getProduct, name="getProduct"),
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
